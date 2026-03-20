5e3736d7c6c7f362d52cf5a73d414e4f464c8858
learngermanghana <learngermanghana@gmail.com>
2026-03-20 21:16:56 +0100

    Delete web/src/services directory

--- web/src/services/grammarService.js
+++
@@ -1,8 +0,0 @@
-import { callAI } from "./aiClient";
-import { getBackendUrl } from "./backendUrl";
-
-const backendUrl = getBackendUrl();
-
-export const askGrammar = (payload) => callAI("/grammar/ask", payload);
-
-export { backendUrl };
